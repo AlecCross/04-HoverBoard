d4f89043dd68e707bf23ebe4773b4fd1df2c9d6a
AlecCross <cross2472@gmail.com>
2021-06-20 00:24:56 +0300

    Рисование rand цветом
    ...с анимацией исчезновения

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const board = document.querySelector('#board')
+const colors = ['magenta','green','orange','yellow']
 const SQUARE_NUMBER = 500//Константа  большими буквами
 
 for(let i = 0; i < SQUARE_NUMBER; i++){
@@ -6,14 +7,27 @@ for(let i = 0; i < SQUARE_NUMBER; i++){
     square.classList.add('square')
 
 square.addEventListener('mouseover', () => {
-    console.log("mouseover")
     setColor(square)
+})
+
+square.addEventListener('mouseleave', () => {
+    removeColor(square)
 })
 
     board.append(square)
 }
 
 function setColor(element){
-    element.style.background = 'red'
+    const color = getRandColor()
+    element.style.background = color
+}
+
+function removeColor(element){
+    element.style.background = '#1d1d1d'
+}
+
+function getRandColor(){
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
 }
 
