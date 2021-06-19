f013014774dd352ace9046c593bc2716d43e2ba5
AlecCross <cross2472@gmail.com>
2021-06-20 00:13:05 +0300

    Рисование цветом
    ...красным с анимацией появления

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const board = document.querySelector('#board')
-const SQUARE_NUMBER = 450//Константа  большими буквами
+const SQUARE_NUMBER = 500//Константа  большими буквами
 
 for(let i = 0; i < SQUARE_NUMBER; i++){
     const square = document.createElement('div')
     square.classList.add('square')
 
+square.addEventListener('mouseover', () => {
+    console.log("mouseover")
+    setColor(square)
+})
+
     board.append(square)
+}
+
+function setColor(element){
+    element.style.background = 'red'
 }
 
